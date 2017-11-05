3e441b9bbde60c0d074464487e73ebda3c8947ea
Ye Tian <ye.tian.stanford@gmail.com>
2017-11-04 21:44:24 -0700

    modified image_min_size to adapt our dataset

--- libs/configs/config_v1.py
+++ libs/configs/config_v1.py
@@ -189,7 +189,7 @@ tf.app.flags.DEFINE_string(
     'The classes to classify.')
 
 tf.app.flags.DEFINE_integer(
-    'image_min_size', 640,
+    'image_min_size', 120,
     'resize image so that the min edge equals to image_min_size')
 
 #####################
